0f02c81bce38d9e58f9992aa43337b76096f9727
norton_vianna <norton.vianna@zooxsmart.com>
2025-11-05 14:20:28 -0300

    alterações requirements

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from routes import router
+from auth_routes import auth_router
 
 app = FastAPI()
     
-from routes import router
-from auth_routes import auth_router
+
 
 app.include_router(router)
 app.include_router(auth_router)
